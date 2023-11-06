0f0d23ea88a323be636afa99d7df74d0e204147e
JMHaryBeccarVarela <julianhary04@gmail.com>
2023-11-06 20:51:48 -0300

    hovered background

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,16 @@
-
-import './App.css'
-
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen  flex items-center justify-center">
+      <div className="fixed flex flex-wrap w-screen h-screen80">
+        {Array.from({ length: 10000 }, (_, index) => (
+          <div
+            key={index}
+            className="w-3 h-3 bg-gray-100 hover:bg-blue-500 transition duration-0"
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
